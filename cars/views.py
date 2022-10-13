f5eb28100821c9d81b37d6f59544fa5c1d99c84a
Abdelaziz <azizmahamat@gmail.com>
2022-10-13 19:27:35 +0200

    Completed search functionnality and admin customization

--- cars/views.py
+++ cars/views.py
@@ -5,11 +5,18 @@ from cars.models import Car
 
 def cars(request):
     cars = Car.objects.order_by('-created_date')
+    search_fields = {
+        'model': list(Car.objects.values_list('model', flat=True).distinct()),
+        'year': list(Car.objects.values_list('year', flat=True).distinct()),
+        'body_style': list(Car.objects.values_list('body_style', flat=True).distinct()),
+        'city': list(Car.objects.values_list('city', flat=True).distinct()),
+    }
     paginator = Paginator(cars, 2)
     page = request.GET.get('page')
     paged_cars = paginator.get_page(page)
     data = {
         'cars': paged_cars,
+        'search_fields': search_fields,
     }
     return render(request, 'cars/cars.html', data)
 
@@ -20,3 +27,50 @@ def car_detail(request, id):
         'single_car': single_car,
     }
     return render(request, 'cars/car_detail.html', data)
+
+
+def search(request):
+    cars = Car.objects.order_by('-created_date')
+
+    search_fields = {
+        'model': list(Car.objects.values_list('model', flat=True).distinct()),
+        'year': list(Car.objects.values_list('year', flat=True).distinct()),
+        'body_style': list(Car.objects.values_list('body_style', flat=True).distinct()),
+        'city': list(Car.objects.values_list('city', flat=True).distinct()),
+        'transmission': list(Car.objects.values_list('transmission', flat=True).distinct()),
+    }
+
+    if 'keyword' in request.GET and request.GET['keyword']:
+        keyword = request.GET['keyword']
+        cars = cars.filter(description__icontains=keyword)
+
+    if 'model' in request.GET and request.GET['model']:
+        model = request.GET['model']
+        cars = cars.filter(model__iexact=model)
+
+    if 'city' in request.GET and request.GET['city']:
+        city = request.GET['city']
+        cars = cars.filter(city__iexact=city)
+
+    if 'body_style' in request.GET and request.GET['body_style']:
+        body_style = request.GET['body_style']
+        cars = cars.filter(body_style__iexact=body_style)
+
+    if 'year' in request.GET and request.GET['year']:
+        year = request.GET['year']
+        cars = cars.filter(year__iexact=year)
+
+    if 'transmission' in request.GET and request.GET['transmission']:
+        transmission = request.GET['transmission']
+        cars = cars.filter(transmission__iexact=transmission)
+
+    if 'min_price' in request.GET and request.GET['max_price']:
+        min_price = request.GET['min_price']
+        max_price = request.GET['max_price']
+        cars = cars.filter(price__gte=min_price, price__lte=max_price)
+
+    data = {
+        'cars': cars,
+        'search_fields': search_fields,
+    }
+    return render(request, 'cars/search.html', data)
--- pages/views.py
+++ pages/views.py
@@ -2,18 +2,28 @@ from django.shortcuts import render
 from .models import Team
 from cars.models import Car
 
-# Create your views here.
 
 def home(request):
     teams = Team.objects.all()
     featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
     all_cars = Car.objects.order_by('-created_date')
+    search_fields = {
+        'model': list(Car.objects.values_list('model', flat=True).distinct()),
+        'year': list(Car.objects.values_list('year', flat=True).distinct()),
+        'body_style': list(Car.objects.values_list('body_style', flat=True).distinct()),
+        'city': list(Car.objects.values_list('city', flat=True).distinct()),
+        'transmission': list(Car.objects.values_list('transmission', flat=True).distinct()),
+    }
+    print("-" * 30)
+    print(search_fields)
     data = {
         'teams': teams,
         'featured_cars': featured_cars,
         'all_cars': all_cars,
+        'search_fields': search_fields,
     }
     return render(request, 'pages/home.html', data)
+
 
 def about(request):
     teams = Team.objects.all()
@@ -21,6 +31,7 @@ def about(request):
         'teams': teams,
     }
     return render(request, 'pages/about.html', data)
+
 
 def services(request):
     return render(request, 'pages/services.html')
